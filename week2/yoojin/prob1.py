86650020b03266760325e25e3392ccd0d597a5c5
hnyoojin <0dbwls21@gmail.com>
2025-04-11 17:19:56 +0900

    prob1 : 푸는 중 중간 업로드

--- week2/yoojin/prob1.py
+++ week2/yoojin/prob1.py
@@ -3,69 +3,121 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-LD, RD, RU, LU = False, False, False, False
-current_route = []
-routes_of_slanted_rect = []
-sum_of_slanted_rect = []
-max_sum_of_slanted_rect = 0
 count_of_slanted_rect = 0
+sum_of_current_rect = 0
+max_sum_of_slanted_rect = 0
+current_route = []
+LD, RD, RU, LU = False, False, False, False
 
-def where_to_go():
+
+def where_to_go(i, j):
+    global LD, RD, RU, LU
+    global count_of_slanted_rect
+    global current_route
+    global max_sum_of_slanted_rect
+
     if LD == False:
-        left_down()
-    elif RD == False:
-        right_down()
-    elif RU == False:
-        right_up()
-    elif LU == False:
-        left_up()
+        left_down(i, j)
+    if LD == True:
+        left_down(i, j)
+        right_down(i, j)
+    elif RD == True:
+        right_down(i, j)
+        left_up(i, j)
+    elif RU == True:
+        right_up(i, j)
+        left_up(i, j)
+    elif LU == True:
+        left_up(i, j)
     else:
         print_result()
 
 def print_result():
-    print(max(sum_of_slanted_rect))
+    global count_of_slanted_rect
+    global current_route
+    global max_sum_of_slanted_rect
+
+    count_of_slanted_rect += 1
+    sum_of_current_rect = sum(current_route)
+    
+    current_route = []
+    max_sum_of_slanted_rect = max(max_sum_of_slanted_rect, sum_of_current_rect)
 
 def left_down(i, j):
+    global LD
+    global current_route
     i += 1
     j -= 1
-    if i>n-1 or j<0: # can't go L/D
-        if LD==True:
-            right_down(i-1, j+1)
-        else: 
+    
+    if i > n-1 or j < 0:
+        if LD == True:
+            where_to_go(i-1, j+1)
+        else:
+            current_route = []
             return
     else:
         current_route.append(grid[i][j])
         LD = True
-        left_down()
-        right_down()
-
+        where_to_go(i, j)
 
 def right_down(i, j):
+    global RD
+    global current_route
     i += 1
-    j -= 1
-    RD = True
-    where_to_go()
+    j += 1
+    
+    if i > n-1 or j > n-1:
+        if RD == True:
+            current_route = []
+            where_to_go(i-1, j-1)
+        else:
+            return
+    else:
+        current_route.append(grid[i][j])
+        RD = True
+        where_to_go(i, j)
+
 
 def right_up(i, j):
-    i += 1
-    j += 1
-    RU = True
-    where_to_go()
-
-def left_up(i, j):
+    global RU
+    global current_route
     i -= 1
     j += 1
-    LU = True
-    where_to_go()
-
-
-# code
-for i range(n):
-    for j range(n):
-        current_route.append(grid[i][j])
-        where_to_go()
-
-if LD and RD and RU and LU == True:
-    max_sum_of_slanted_rect = max(max_sum_of_slanted_rect, sum_of_slanted_rect)
-    LD, RD, RU, LU = False, False, False, False
     
+    if i < 0 or j > n-1:
+        if RU == True:
+            current_route = []
+            where_to_go(i+1, j-1)
+        else:
+            return
+    else:
+        current_route.append(grid[i][j])
+        RU = True
+        where_to_go(i, j)
+
+
+def left_up(i, j):
+    global LU
+    global current_route
+    i -= 1
+    j -= 1
+    
+    if i < 0 or j < 0:
+        if LU == True:
+            current_route = []
+            where_to_go(i+1, j+1)
+        else:
+            return
+    else:
+        current_route.append(grid[i][j])
+        LU = True
+        where_to_go(i, j)
+
+
+for i in range(n):
+    for j in range(n):
+        current_route.append(grid[i][j])
+        LD, RD, RU, LU = False, False, False, False
+        where_to_go(i, j)
+
+print(max_sum_of_slanted_rect)
